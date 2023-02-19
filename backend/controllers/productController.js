4f70e372b4ca0b27ec9d33353dbaa98a112a81ea
NabajyotiModak <naba93981@gmail.com>
2023-02-19 21:48:48 +0530

    product conttrollers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,83 @@
-const getAllProducts = (req, res)=>{
-    res.status(200).json({message: "Route is working fine"})
+const productCollection = require('../models/ProductModel')
+const { StatusCodes } = require('http-status-codes')
+
+
+const getAllProducts = async (req, res, next) => {
+    const products = await productCollection.find()
+    res.status(StatusCodes.OK).json({
+        success: true,
+        products
+    })
 }
 
-module.exports=getAllProducts
+const getOneProduct = async (req, res, next)=>{
+    const { id: productID } = req.params;
+    const product = await productCollection.findById({_id: productID});
+    if (!product) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    res.status(StatusCodes.OK).json({
+        success: true,
+        product
+    })
+}
+
+// create product - by admin
+const createProduct = async (req, res, next) => {
+    const product = await productCollection.create(req.body)
+    res.status(StatusCodes.CREATED).json({
+        success: true,
+        product
+    })
+}
+
+const updateProduct = async (req, res, next) => {
+    const { id: productID } = req.params;
+    const product = await productCollection.findOneAndUpdate(
+        { _id: productID },
+        req.body,
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+
+    if (!product) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    res.status(StatusCodes.OK).json({
+        success: true,
+        product
+    })
+}
+
+const deleteProduct = async (req, res, next) => {
+    const { id: productID } = req.params;
+    const product = await productCollection.findOneAndDelete({ _id: productID })
+
+    if (!product) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    res.status(StatusCodes.OK).json({
+        success: true,
+        product
+    })
+}
+module.exports = {
+    createProduct,
+    getAllProducts,
+    updateProduct,
+    deleteProduct,
+    getOneProduct
+}
