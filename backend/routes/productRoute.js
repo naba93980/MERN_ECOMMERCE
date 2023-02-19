4bc9dbfc303d499fb3c4f82865e08f7dc037bf82
NabajyotiModak <naba93981@gmail.com>
2023-02-19 21:47:56 +0530

    added post, put, delete, get routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router=express.Router();
-const getAllProducts = require('../controllers/productController')
+const {getAllProducts, createProduct, updateProduct, deleteProduct, getOneProduct} = require('../controllers/productController')
 
 router.route('/products').get(getAllProducts)
+router.post('/product/new',createProduct)
+router.route('/product/:id').put(updateProduct).delete(deleteProduct).get(getOneProduct)
 
 module.exports=router
