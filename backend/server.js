ee98c90dd43c4a2cad056e8d621bf537284fcd2b
NabajyotiModak <naba93981@gmail.com>
2023-02-21 22:22:30 +0530

    process exits on uncaught exception and unhandled rejection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
-const app = require('./app')
-
 require('dotenv').config({path: "backend/config/config.env"});
-
+const app = require('./app')
 const PORT = process.env.PORT;
 const connectDB = require('./database/connect')
+
+// Handling  Uncaught Exception 
+process.on("uncaughtException",(err)=>{
+  console.log(`Shutting down server due to UncaughtException`)
+  process.exit(1);
+})
+
+// Handling  Promise Rejection
+process.on("unhandledRejection",(err)=>{
+  console.log(`Shutting down server due to UnhandledRejection`)
+  process.exit(1);
+})
 
 const start = async () => {
     try {
